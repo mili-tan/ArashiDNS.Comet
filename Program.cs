942ee58c57320fba4951d6e0df2e70a001829775
Milkey Tan <24996957+mili-tan@users.noreply.github.com>
2024-12-06 19:49:33 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using ARSoft.Tools.Net.Dns;
+using System.Net;
+
+namespace ArashiDNS.Comet
+{
+    internal class Program
+    {
+        public static IPAddress Server = IPAddress.Parse("223.5.5.5");
+        public static IPEndPoint ListenerEndPoint = new(IPAddress.Loopback, 23353);
+
+        static void Main(string[] args)
+        {
+            var dnsServer = new DnsServer(new UdpServerTransport(ListenerEndPoint),
+                new TcpServerTransport(ListenerEndPoint));
+            dnsServer.QueryReceived += DnsServerOnQueryReceived;
+            dnsServer.Start();
+
+            Console.WriteLine("Now listening on: " + ListenerEndPoint);
+            Console.WriteLine("Application started. Press Ctrl+C / q to shut down.");
+            if (!Console.IsInputRedirected && Console.KeyAvailable)
+            {
+                while (true)
+                    if (Console.ReadKey().KeyChar == 'q')
+                        Environment.Exit(0);
+            }
+
+            EventWaitHandle wait = new AutoResetEvent(false);
+            while (true) wait.WaitOne();
+        }
+
+        private static async Task DnsServerOnQueryReceived(object sender, QueryReceivedEventArgs e)
+        {
+            if (e.Query is not DnsMessage query || query.Questions.Count == 0) return;
+            var quest = query.Questions.First();
+            var nsRecord = new DnsClient(Server, 10000).Resolve(quest.Name, RecordType.Ns).AnswerRecords.First();
+            var nsServerName = ((NsRecord)nsRecord).NameServer;
+
+            var nsARecord = new DnsClient(Server, 10000).Resolve(nsServerName)?.AnswerRecords.First();
+            var nsAddress = ((ARecord)nsARecord).Address;
+
+            var response = await new DnsClient(nsAddress, 10000).ResolveAsync(quest.Name, quest.RecordType);
+            response.TransactionID = e.Query.TransactionID;
+            e.Response = response;
+        }
+    }
+}
